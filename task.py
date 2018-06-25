44ac2a4727d066e39f00838b9fbcfea52b8a1023
eslutskaya <eslutskaya@gmail.com>
2018-06-25 13:58:57 +0300

    done!

--- task.py
+++ task.py
@@ -2,61 +2,68 @@ from tkinter import *
 import functools
 
 @functools.singledispatch
-def Name(a):
-    type_name = type(a).__name__
-    assert False, "Unsupported type : " + type_name
+def perevod(a):
+    type_name = type(arg).__name__
+    assert False, "Unsupported notation : " + type_name
 
-@Name.register(int)
+@perevod.register(int)
 def _(a):
-    if (a == 0):
+    if a == 0:
         return 'Ноль'
-    elif (a == 1):
+    if a == 1:
         return 'Один'
-    elif (a == 2):
+    if a == 2:
         return 'Два'
-    elif (a == 3):
+    if a == 3:
         return 'Три'
-    elif (a == 4):
+    if a == 4:
         return 'Четыре'
-    elif (a == 5):
+    if a == 5:
         return 'Пять'
-    elif (a == 6):
+    if a == 6:
         return 'Шесть'
-    elif (a == 7):
+    if a == 7:
         return 'Семь'
-    elif (a == 8):
+    if a == 8:
         return 'Восемь'
-    elif (a == 9):
+    if a == 9:
         return 'Девять'
+    else:
+        return 'Данная функция доступна только от чисел от 0 до 9'
 
-@Name.register(str)
+
+@perevod.register(str)
 def _(a):
-    return a
+    return a[2::]
 
 def butt():
     try:
         dt = digType.get()
         d = int(dig.get())
         if dt == 'int':
-            res = (d)
+            res = perevod(d)
         if dt == 'bin':
-            res = Name(bin(d))
+            res = perevod(bin(d))
         if dt == 'oct':
-            res = Name(oct(d))
+            res = perevod(oct(d))
         if dt == 'hex':
-            res = Name(hex(d))
+            res = perevod(hex(d))
         result = Label(root, bg="steelblue3", text=res, width = '200', fg='gray1')
         result.pack()
     except ValueError as e:
-        result = Label(root, bg="red", text="Введите ЦИФРУ", width = '200', fg='gray1')
-        result.pack()
+        result = Label(root, bg="red", text="Введите ЧИСЛО", width = '200', fg='gray1')
+        result.pack()    
 
 
 root = Tk()
+root.geometry('500x400+300+200')
+root.title("Экзаменационное задание")
+
 dig = Entry(root)
 dig.pack()
 digType = StringVar()
-rbutton1=Radiobutton(root,text='int',variable=digType,value='int')
+
+rbutton1=Radiobutton(root,text='Имя',variable=digType,value='int')
 rbutton2=Radiobutton(root,text='bin',variable=digType,value='bin')
 rbutton3=Radiobutton(root,text='oct',variable=digType,value='oct')
 rbutton4=Radiobutton(root,text='hex',variable=digType,value='hex')
@@ -64,7 +71,7 @@ rbutton1.pack()
 rbutton2.pack()
 rbutton3.pack()
 rbutton4.pack()
-button = Button(root, bg="blue", fg='gray1', text="Преобразовать", command=butt)
+button = Button(root, bg="blue", fg='gray1', text="Перевод", command=butt)
 button.pack()
-root.geometry('500x400+300+200')
+
 root.mainloop()
